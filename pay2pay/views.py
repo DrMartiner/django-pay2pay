873aa4ac785534964ed250ad4e96f26f3df4df05
DrMartiner <DrMartiner@GMail.Com>
2013-06-29 19:32:46 +0700

    Use the logger

--- pay2pay/views.py
+++ pay2pay/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import conf
-import xmltodict
 import base64
+import logging
+import xmltodict
 import lxml.etree
 import lxml.builder
 from django.http import HttpResponse
@@ -11,6 +12,8 @@ from annoying.functions import get_object_or_None
 from django.views.decorators.csrf import csrf_exempt
 from .utils import get_signature
 from .models import Order
+
+logger = logging.getLogger('pay2pay')
 
 
 class Confirm(View):
@@ -24,8 +27,10 @@ class Confirm(View):
         xml = request.POST.get('xml', '')
         xml = base64.b64decode(xml)
 
-        # TODO: make try\catch
-        response = self._get_obj_response(xml)
+        try:
+            response = self._get_obj_response(xml)
+        except (ValueError, TypeError):
+            logger.error('Can not parse xml', exc_info=True)
 
         order = get_object_or_None(Order, order_id=response.get('order_id'))
         if order:
@@ -33,17 +38,23 @@ class Confirm(View):
             sign = get_signature(xml, conf.PAY2PAY_HIDE_KEY)
             if sig_encode == sign:
                 # TODO: make try\catch
-                order.status = response['status']
-                order.paymode = response['paymode']
-                order.trans_id = response['trans_id']
-                order.error_msg = response.get('error_msg', '')
-                if response.get('test_mode', '') == '1':
-                    order.test_mode = True
-                order.save()
+                try:
+                    order.status = response['status']
+                    order.paymode = response['paymode']
+                    order.trans_id = response['trans_id']
+                    order.error_msg = response.get('error_msg', '')
+                    if response.get('test_mode', '') == '1':
+                        order.test_mode = True
+                    order.save()
+                except KeyError:
+                    logger.error('Have not some key at parsed XML data', exc_info=True)
             else:
                 error_msg = 'Security check failed'
         else:
             error_msg = 'Unknown order_id'
+
+        if error_msg:
+            logger.error(error_msg, exc_info=True)
 
         xml_response = self._get_xml_response(error_msg)
         return HttpResponse(xml_response)
