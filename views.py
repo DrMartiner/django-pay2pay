cb1249e7c108431141db702d6caa7f4a1267a146
DrMartiner <DrMartiner@GMail.Com>
2013-07-09 14:26:34 +0700

    Move to app templates

--- views.py
+++ views.py
@@ -2,11 +2,10 @@
 
 from django.views.generic import TemplateView
 from pay2pay.forms import PayForm
-from pay2pay.models import Payment
 
 
 class Paypage(TemplateView):
-    template_name = 'pay_page.html'
+    template_name = 'pay2pay/pay_page.html'
 
     def get_context_data(self, **kwargs):
         ctx = super(Paypage, self).get_context_data(**kwargs)
